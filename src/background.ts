ff90f4e00620a3e50aa5a91ea16e71300b687165
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-30 18:09:20 +0100

    Update background.ts

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,21 @@
 chrome.runtime.onMessage.addListener((request) => {
   const { timeSpent, currUrl, title, faviconUrl } = request;
-  chrome.storage.local.set({ [title]: { timeSpent: timeSpent, title: title, url: currUrl, faviconUrl: faviconUrl } });
+
+  console.log("Received message from content script:", request);
+
+  chrome.storage.local.get([title], (result) => {
+    const storedData = result[title];
+
+    const newTimeSpent = storedData ? storedData.timeSpent + timeSpent : timeSpent;
+
+    // Set or update the activity data in storage
+    chrome.storage.local.set({
+      [title]: {
+        timeSpent: newTimeSpent,
+        title,
+        url: currUrl,
+        faviconUrl,
+      },
+    });
+  });
 });
