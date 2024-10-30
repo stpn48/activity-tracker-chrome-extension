67a5fc18cdc4f6831e8a08c1230d7a2e458f8781
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-30 18:09:23 +0100

    Create formatTime.ts

---
+++ src/utils/formatTime.ts
@@ -0,0 +1,7 @@
+export function formatTime(seconds: number) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  return `${hours ? `${hours}h` : ""} ${minutes ? `${minutes}m` : ""} ${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}s`;
+}
