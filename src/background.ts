895dcb43eb2f4b7132a5d31fe7bec0e8a571cc6e
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-01 06:33:43 +0100

    Update background.ts

--- src/background.ts
+++ src/background.ts
@@ -1,21 +1,46 @@
+import { v4 as uuidv4 } from "uuid";
+import { Activity } from "./types/types";
+
 chrome.runtime.onMessage.addListener((request) => {
   const { timeSpent, currUrl, title, faviconUrl } = request;
 
   console.log("Received message from content script:", request);
 
   chrome.storage.local.get([title], (result) => {
-    const storedData = result[title];
+    const storedData: Activity | undefined = result[title];
 
+    const uuid = storedData ? storedData.id : uuidv4();
     const newTimeSpent = storedData ? storedData.timeSpent + timeSpent : timeSpent;
+
+    const activity: Activity = {
+      id: uuid,
+      timeSpent: newTimeSpent,
+      title,
+      url: currUrl,
+      faviconUrl,
+    };
 
     // Set or update the activity data in storage
     chrome.storage.local.set({
-      [title]: {
-        timeSpent: newTimeSpent,
-        title,
-        url: currUrl,
-        faviconUrl,
-      },
+      [title]: activity,
     });
   });
+});
+
+chrome.alarms.clearAll();
+
+chrome.alarms.get("clearStorageAtMidnight", (alarm) => {
+  if (alarm) return;
+
+  chrome.alarms.create("clearStorageAtMidnight", {
+    periodInMinutes: 1440,
+  });
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "clearStorageAtMidnight") {
+    chrome.storage.local.clear(() => {
+      console.log("Storage cleared at midnight");
+    });
+  }
 });
