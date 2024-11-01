ecef45df0b8c786335bd756928ce42cad820f6f5
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-01 06:33:40 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,16 @@
-import { useCallback, useEffect, useState } from "react";
-import { formatTime } from "./utils/formatTime";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { FocusPage } from "./components/FocusPage";
+import MainPage from "./components/MainPage";
 
-type Activity = {
-  timeSpent: number;
-  title: string;
-  url: string;
-  faviconUrl: string;
-};
-
-function App() {
-  const [activities, setActivities] = useState<Activity[]>([]); // Initialize as an empty array
-  const [totalTimeSpent, setTotalTimeSpent] = useState(0); // Initialize as 0
-
-  const getActivities = useCallback(() => {
-    chrome.storage.local.get(null, (result) => {
-      const localStorage = result as { [title: string]: Activity };
-      setActivities(Object.values(localStorage).sort((a, b) => b.timeSpent - a.timeSpent)); // Sort by timeSpent in descending order
-      setTotalTimeSpent(Object.values(localStorage).reduce((acc, curr) => acc + curr.timeSpent, 0));
-    });
-  }, []);
-
-  useEffect(() => {
-    getActivities(); // Initial fetch
-    chrome.storage.onChanged.addListener(getActivities); // Update on storage change
-
-    // Cleanup listener on component unmount
-    return () => chrome.storage.onChanged.removeListener(getActivities);
-  }, [getActivities]);
-
+export default function App() {
   return (
-    <div className="flex flex-col gap-4 w-[400px] bg-[#131313] p-4 text-white">
-      <h1>Total Time Spent Today: {formatTime(totalTimeSpent)}</h1>
-      {activities.map((activity, index) => (
-        <div className="flex gap-2 justify-between items-center" key={index}>
-          <div className="flex items-center gap-4">
-            <img width={24} className="rounded-full" height={24} src={activity.faviconUrl} alt="favicon" />
-            <h1>{activity.title}</h1>
-          </div>
-          <h1>{formatTime(activity.timeSpent)}</h1>
-        </div>
-      ))}
-    </div>
+    <HashRouter>
+      <div className="font-geistSans w-[400px] text-sm bg-[#131313] p-8 text-white">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/focus" element={<FocusPage />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
-
-export default App;
