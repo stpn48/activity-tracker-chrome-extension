277cbed1116d309fe36377d124be4b39249102b9
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-02 09:49:06 +0100

    Update content.ts

--- src/content.ts
+++ src/content.ts
@@ -1,28 +1,38 @@
 import { getFaviconUrl } from "./utils/getFaviconUrl";
 
 let intervalId: NodeJS.Timeout | null = null;
+let observer: MutationObserver | null = null;
 
-function sendDataToBackground() {
-  const title = window.location.hostname;
-  const faviconUrl = getFaviconUrl();
-  const currUrl = window.location.href;
-  const timeSpent = 1; // 1 second
-
+// Activity tracking functions
+function sendActivityData() {
   chrome.runtime.sendMessage({
-    currUrl,
-    timeSpent,
-    title,
-    faviconUrl,
+    type: "activityData",
+    currUrl: window.location.href,
+    title: window.location.hostname,
+    faviconUrl: getFaviconUrl(),
   });
 }
 
+function incrementSessionCount(count: number) {
+  chrome.runtime.sendMessage({
+    type: "incrementSessionCount",
+    title: window.location.hostname,
+    sessionCount: count,
+  });
+}
+
+function incrementTimeSpent(time: number) {
+  chrome.runtime.sendMessage({
+    type: "incrementTimeSpent",
+    title: window.location.hostname,
+    time,
+  });
+}
+
+// Tracking control functions
 function startTracking() {
   if (!intervalId) {
-    intervalId = setInterval(sendDataToBackground, 1000);
-  } else {
-    clearInterval(intervalId);
-    intervalId = null;
-    intervalId = setInterval(sendDataToBackground, 1000);
+    intervalId = setInterval(() => incrementTimeSpent(1), 1000);
   }
 }
 
@@ -31,16 +41,78 @@ function stopTracking() {
     clearInterval(intervalId);
     intervalId = null;
   }
-  sendDataToBackground(); // Final data send on unload
 }
 
-window.addEventListener("load", startTracking);
-window.addEventListener("beforeunload", stopTracking);
+// Thumbnail blurring functions
+function blurYtThumbnails(state: boolean) {
+  const thumbnails = document.querySelectorAll(
+    "img.yt-core-image.yt-core-image--fill-parent-height.yt-core-image--fill-parent-width.yt-core-image--content-mode-scale-aspect-fill.yt-core-image--loaded",
+  );
 
-document.addEventListener("visibilitychange", () => {
-  if (document.visibilityState === "hidden") {
-    stopTracking();
-  } else if (document.visibilityState === "visible") {
+  // If state is false, remove blur from all thumbnails
+  if (!state) {
+    thumbnails.forEach((thumbnail) => {
+      (thumbnail as HTMLElement).style.filter = "";
+    });
+    return;
+  }
+
+  thumbnails.forEach((thumbnail) => {
+    (thumbnail as HTMLElement).style.filter = "blur(50px)";
+  });
+}
+
+function startObserver() {
+  if (!observer) {
+    observer = new MutationObserver(() => {
+      blurYtThumbnails(true); // Call blur with state true
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
+}
+
+function stopObserver() {
+  if (observer) {
+    observer.disconnect();
+    observer = null;
+  }
+}
+// Initialize observers and event listeners
+function init() {
+  window.addEventListener("load", () => {
+    sendActivityData();
+    incrementSessionCount(1);
     startTracking();
+  });
+
+  window.addEventListener("beforeunload", stopTracking);
+
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+      stopTracking();
+    } else if (document.visibilityState === "visible") {
+      incrementSessionCount(1);
+      startTracking();
+    }
+  });
+
+  chrome.storage.local.onChanged.addListener((changes) => {
+    if (changes.focusState.newValue.blurYoutubeThumbnails) {
+      blurYtThumbnails(true);
+      startObserver();
+    } else {
+      blurYtThumbnails(false);
+      stopObserver();
+    }
+  });
+}
+
+chrome.storage.local.get("focusState", (result) => {
+  if (result.focusState.blurYoutubeThumbnails) {
+    blurYtThumbnails(true);
+    startObserver();
   }
 });
+
+// Start the script
+init();
