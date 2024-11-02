323ae54a21d2878d9cb54b7edc426bf30d0b6d73
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-02 22:22:10 +0100

    Create trackTimeSpent.ts

---
+++ src/utils/trackTimeSpent.ts
@@ -0,0 +1,41 @@
+import { getFaviconUrl } from "@/utils/getFaviconUrl";
+
+let intervalId: NodeJS.Timeout | null = null;
+
+export function incrementSessionCount(count: number) {
+  chrome.runtime.sendMessage({
+    type: "incrementSessionCount",
+    title: window.location.hostname,
+    sessionCount: count,
+  });
+}
+
+export function incrementTimeSpent(time: number) {
+  chrome.runtime.sendMessage({
+    type: "incrementTimeSpent",
+    title: window.location.hostname,
+    time,
+  });
+}
+
+export function startTracking() {
+  if (!intervalId) {
+    intervalId = setInterval(() => incrementTimeSpent(1), 1000);
+  }
+}
+
+export function stopTracking() {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+}
+
+export function sendActivityData() {
+  chrome.runtime.sendMessage({
+    type: "activityData",
+    currUrl: window.location.href,
+    title: window.location.hostname,
+    faviconUrl: getFaviconUrl(),
+  });
+}
