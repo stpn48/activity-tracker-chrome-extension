223f9c9a856b0b394d63147babbc404ccb0026f2
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-02 22:27:28 +0100

    Update ActivityCard.tsx

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Activity } from "../types/types";
 import { formatTime } from "../utils/formatTime";
 
@@ -7,17 +8,25 @@ type Props = {
 
 export function ActivityCard({ activity }: Props) {
   return (
-    <div className="flex px-4 rounded-full cursor-pointer py-2 hover:bg-stone-600 gap-2 justify-between items-center">
-      <div className="flex items-center gap-4">
-        <img width={24} className="rounded-full" height={24} src={activity.faviconUrl} alt="favicon" />
-        <div>
-          <h1 className="truncate max-w-[150px]"> {activity.title.replace(/^www\./, "")}</h1>
-          <p className="text-xs text-stone-500">
-            {activity.sessionCount} {activity.sessionCount === 1 ? "session" : "sessions"}
-          </p>
+    <Link to={`/activity/${activity.id}`}>
+      <div className="flex cursor-pointer items-center justify-between gap-2 rounded-full px-4 py-2 hover:bg-stone-600">
+        <div className="flex items-center gap-4">
+          <img
+            width={24}
+            className="rounded-full"
+            height={24}
+            src={activity.faviconUrl}
+            alt="favicon"
+          />
+          <div>
+            <h1 className="max-w-[150px] truncate"> {activity.title.replace(/^www\./, "")}</h1>
+            <p className="text-xs text-stone-500">
+              {activity.sessionCount} {activity.sessionCount === 1 ? "session" : "sessions"}
+            </p>
+          </div>
         </div>
+        <h1 className="font-geistMono">{formatTime(activity.timeSpent)}</h1>
       </div>
-      <h1 className="font-geistMono">{formatTime(activity.timeSpent)}</h1>
-    </div>
+    </Link>
   );
 }
