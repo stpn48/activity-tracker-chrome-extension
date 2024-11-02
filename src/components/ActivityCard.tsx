be7a7e92a172d9b00d9451751999ff5bff3cce65
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-02 09:49:03 +0100

    Update ActivityCard.tsx

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -10,7 +10,12 @@ export function ActivityCard({ activity }: Props) {
     <div className="flex px-4 rounded-full cursor-pointer py-2 hover:bg-stone-600 gap-2 justify-between items-center">
       <div className="flex items-center gap-4">
         <img width={24} className="rounded-full" height={24} src={activity.faviconUrl} alt="favicon" />
-        <h1 className="truncate max-w-[150px]">{activity.title}</h1>
+        <div>
+          <h1 className="truncate max-w-[150px]"> {activity.title.replace(/^www\./, "")}</h1>
+          <p className="text-xs text-stone-500">
+            {activity.sessionCount} {activity.sessionCount === 1 ? "session" : "sessions"}
+          </p>
+        </div>
       </div>
       <h1 className="font-geistMono">{formatTime(activity.timeSpent)}</h1>
     </div>
