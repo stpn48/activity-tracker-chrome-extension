990af79de36a312477a53191e0cd6d01c8892180
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-02 09:49:02 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import MainPage from "./components/MainPage";
 export default function App() {
   return (
     <HashRouter>
-      <div className="font-geistSans w-[400px] text-sm bg-[#131313] p-8 text-white">
+      <div className="font-geistSans rounded-lg w-[400px] text-sm bg-[#131313] p-8 text-white">
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/focus" element={<FocusPage />} />
