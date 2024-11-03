c3ff11e6834d1040b233e3922511641ec0f67b98
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-03 17:28:40 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
+import { ActivityPage } from "@/components/ActivityPage";
 import { FocusPage } from "@/components/FocusPage";
 import MainPage from "@/components/MainPage";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes, useLocation } from "react-router-dom";
+
+function PageWrapper({ children }: { children: React.ReactNode }) {
+  const location = useLocation();
+  const isActivityPage = location.pathname.startsWith("/activity");
+
+  return (
+    <div
+      className={`rounded-lg bg-[#131313] p-8 font-geistSans text-sm text-white ${
+        isActivityPage ? "w-[800px]" : "w-[400px]"
+      }`}
+    >
+      {children}
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <HashRouter>
-      <div className="w-[400px] rounded-lg bg-[#131313] p-8 font-geistSans text-sm text-white">
+      <PageWrapper>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/focus" element={<FocusPage />} />
-          <Route path="/activity/[id]" element=} /> // TODO: Add activity page with graph timeStamp array should be taken care of GN BRO KEEP WORKIN !! TRUST IN JESUS
+          <Route path="/activity/:id" element={<ActivityPage />} />
         </Routes>
-      </div>
+      </PageWrapper>
     </HashRouter>
   );
 }
